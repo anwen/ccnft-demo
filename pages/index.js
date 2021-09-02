ff78b23c7b6ff99bbaba33023191d11f2d1c9c24
askender <askender43@gmail.com>
2021-09-03 02:43:59 +0800

    test add_meta simple 5 ok

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,9 @@ export default function Home() {
     loadNFTs()
   }, [])
   async function loadNFTs() {    
-    const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.matic.today")
+    const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com")
+    // const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.matic.today")
+    // const provider = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.infura.io/v3/6d993cb640374f1b8baf01f5eddaed8e")
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems()
