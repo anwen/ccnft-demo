37b5bb8b8dcdc5dfe8127ceaaab62449d0ec4434
askender <askender43@gmail.com>
2021-09-07 01:55:29 +0800

    test add_meta simple 9

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './page/**/*.js',
   ],
-  darkMode: true, // false  or 'media' or 'class'
+  darkMode: 'class', // false  or 'media' or 'class'
   theme: {
     extend: {},
   },
