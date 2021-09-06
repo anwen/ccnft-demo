06a1895fe12a7ba3b6e615beca264ba6502e0ad9
askender <askender43@gmail.com>
2021-09-07 01:14:21 +0800

    test add_meta simple 6

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "mumbai",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    /*
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
+      // url: "https://rpc-mumbai.matic.today",
+      url: "https://rpc-mumbai.maticvigil.com",
       accounts: [privateKey]
     },
     matic: {
@@ -22,7 +22,6 @@ module.exports = {
       url: "https://rpc-mainnet.maticvigil.com",
       accounts: [privateKey]
     }
-    */
   },
   solidity: {
     version: "0.8.4",
