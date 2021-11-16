637f9a4bd53afc3d90fb5285a7eaa8ef33e640a0
askender <askender43@gmail.com>
2021-11-16 15:50:07 +0800

    add change-tab css style, fix

--- pages/articles.js
+++ pages/articles.js
@@ -2,6 +2,7 @@ import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from "web3modal"
+import { useRouter } from 'next/router'
 
 import {
   nftmarketaddress, nftaddress
