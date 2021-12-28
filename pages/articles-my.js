6ce46a48aed796b0af029b077a800fc1253a394d
askender <askender43@gmail.com>
2021-12-28 21:15:16 +0800

    file format

--- pages/articles-my.js
+++ pages/articles-my.js
@@ -1,29 +1,27 @@
-import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/router'
+import { ethers } from "ethers"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { useRouter } from "next/router"
 
-import {
-  nftmarketaddress, nftaddress
-} from '../config'
+import { nftmarketaddress, nftaddress } from "../config"
 
 let ethAccount
 export default function MyAssets() {
   const [nfts, setNfts] = useState([])
-  const [loadingState, setLoadingState] = useState('not-loaded')
+  const [loadingState, setLoadingState] = useState("not-loaded")
   const router = useRouter()
 
-  if (router.pathname == '/articles-my') {
-    if (typeof document !== 'undefined') {
-      var els = document.getElementsByClassName("_nav");
-      Array.prototype.forEach.call(els, function(el) {
-          el.classList.remove('current');
-      });
-      document.getElementById("_articles_my").classList.add('current');
+  if (router.pathname == "/articles-my") {
+    if (typeof document !== "undefined") {
+      var els = document.getElementsByClassName("_nav")
+      Array.prototype.forEach.call(els, function (el) {
+        el.classList.remove("current")
+      })
+      document.getElementById("_articles_my").classList.add("current")
     }
   }
-  if (typeof window !== 'undefined') {
-      ethAccount = localStorage.getItem("ethAccount");
+  if (typeof window !== "undefined") {
+    ethAccount = localStorage.getItem("ethAccount")
   }
 
   useEffect(() => {
@@ -33,36 +31,38 @@ export default function MyAssets() {
     const dweb_search_url = `https://dweb-search-api.anwen.cc/get_meta?eth=${ethAccount}`
     console.log(dweb_search_url)
     const ret = await axios.get(dweb_search_url) // TODO
-    console.log(ret);
-    if ("data" in ret.data){
+    console.log(ret)
+    if ("data" in ret.data) {
       setNfts(ret.data.data)
     }
-    setLoadingState('loaded')
+    setLoadingState("loaded")
   }
-  if (loadingState === 'loaded' && !nfts.length) return (
-    <h1 className="py-10 px-20 text-3xl">0 creations</h1>
-  )
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="py-10 px-20 text-3xl">0 creations</h1>
 
   return (
     <div className="flex justify-center">
       <div className="p-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {
-            nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="rounded" />
-                <div className="p-4">
-                  <a href={"/article?cid="+nft.path} >
-                  <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
-                  </a>
-                  <p className="text-2xl font-semibold">By: 
-                  <a href={"/articles?author="+nft.eth} >{nft.authors}</a>
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} className="rounded" />
+              <div className="p-4">
+                <a href={"/article?cid=" + nft.path}>
+                  <p
+                    style={{ height: "64px" }}
+                    className="text-2xl font-semibold"
+                  >
+                    {nft.name}
                   </p>
-                </div>
+                </a>
+                <p className="text-2xl font-semibold">
+                  By:
+                  <a href={"/articles?author=" + nft.eth}>{nft.authors}</a>
+                </p>
               </div>
-
-            ))
-          }
+            </div>
+          ))}
         </div>
       </div>
     </div>
