cc8768aa3f0cf87787978f20c6df5ca6e672da11
askender <askender43@gmail.com>
2022-01-11 16:49:01 +0800

    add: view articles by tag

--- pages/articles.js
+++ pages/articles.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/router"
 import { nftmarketaddress, nftaddress } from "../config"
 
 let ethAccount
+let tag
 export default function MyAssets() {
   const [nfts, setNfts] = useState([])
   const [loadingState, setLoadingState] = useState("not-loaded")
@@ -17,22 +18,34 @@ export default function MyAssets() {
   if ("author" in router.query) {
     ethAccount = router.query.author
     console.log("ethAccount", ethAccount)
-    if (ethAccount == "me") {
+    if (ethAccount == "me") { // TODO: seems useless
       if (typeof window !== "undefined") {
         ethAccount = localStorage.getItem("ethAccount")
       }
     }
+  } else if  ("tag" in router.query) {
+    tag = router.query.tag
+    console.log("tag", tag)
   }
+
   useEffect(() => {
     loadNFTs()
-  }, [ethAccount])
+  }, [ethAccount, tag])
   async function loadNFTs() {
+    // TODO: both author and tag
     if (ethAccount) {
       const dweb_search_url = `https://dweb-search-api.anwen.cc/get_meta?eth=${ethAccount}`
-      console.log(dweb_search_url, "222")
       const ret = await axios.get(dweb_search_url) // TODO
       console.log(ret)
-      // setNfts(items)
+      if ("data" in ret.data) {
+        setNfts(ret.data.data)
+      }
+      setLoadingState("loaded")
+    } else if (tag) {
+      const dweb_search_url = `https://dweb-search-api.anwen.cc/get_meta?tag=${tag}`
+      console.log('dweb_search_url', dweb_search_url)
+      const ret = await axios.get(dweb_search_url) // TODO
+      console.log(ret)
       if ("data" in ret.data) {
         setNfts(ret.data.data)
       }
