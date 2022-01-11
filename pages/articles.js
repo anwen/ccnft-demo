8a9527599b4b4ae3f3e4b7af46b7c71c00d6d2c0
askender <askender43@gmail.com>
2022-01-11 17:25:58 +0800

    add: view articles by tag, add links

--- pages/articles.js
+++ pages/articles.js
@@ -2,7 +2,6 @@ import { ethers } from "ethers"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { useRouter } from "next/router"
-
 import { nftmarketaddress, nftaddress } from "../config"
 
 let ethAccount
@@ -14,16 +13,17 @@ export default function MyAssets() {
   const router = useRouter()
 
   console.log(router.query)
-  
+
   if ("author" in router.query) {
     ethAccount = router.query.author
     console.log("ethAccount", ethAccount)
-    if (ethAccount == "me") { // TODO: seems useless
+    if (ethAccount == "me") {
+      // TODO: seems useless
       if (typeof window !== "undefined") {
         ethAccount = localStorage.getItem("ethAccount")
       }
     }
-  } else if  ("tag" in router.query) {
+  } else if ("tag" in router.query) {
     tag = router.query.tag
     console.log("tag", tag)
   }
@@ -43,7 +43,7 @@ export default function MyAssets() {
       setLoadingState("loaded")
     } else if (tag) {
       const dweb_search_url = `https://dweb-search-api.anwen.cc/get_meta?tag=${tag}`
-      console.log('dweb_search_url', dweb_search_url)
+      console.log("dweb_search_url", dweb_search_url)
       const ret = await axios.get(dweb_search_url) // TODO
       console.log(ret)
       if ("data" in ret.data) {
@@ -62,15 +62,21 @@ export default function MyAssets() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {nfts.map((nft, i) => (
             <div key={i} className="border shadow rounded-xl overflow-hidden">
-              <img src={nft.image} className="rounded" />
+              <a href={"/article?cid=" + nft.path}>
+                <img src={nft.image} className="rounded" />
+              </a>
               <div className="p-4">
                 <a href={"/article?cid=" + nft.path}>
                   <p className="text-2xl font-semibold">{nft.name}</p>
                 </a>
                 <p className="text-2xl font-semibold">
-                  By:
+                  By: &nbsp;
                   <a href={"/articles?author=" + nft.eth}>{nft.authors}</a>
                 </p>
+                Tags: &nbsp;
+                {nft.tags.map((tag, i) => (
+                  <a href={"/articles?tag=" + tag}>{tag}</a>
+                ))}
               </div>
             </div>
           ))}
